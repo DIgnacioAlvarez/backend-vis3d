12e0c3775e0e94855611ef0072f4e2f0e5ae374c
SantiagoVillagra <sanmavillagra@gmail.com>
2024-09-27 16:52:23 -0300

    corregido handler name

--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -1,11 +1,10 @@
 import { Router } from "express";
-import {getAllProducts,postProductHandler,getProductByIdHandler,getByNameHandler} from '../handlers/productsHandler.js'
+import {getAllProducts,postProductHandler,getProductByIdHandler,} from '../handlers/productsHandler.js'
 console.log("antesExport")
 
 export const productRoutes = Router()
 console.log("despuesExport")
 productRoutes.get("/:id", async (req,res) => await getProductByIdHandler(req,res) )
 productRoutes.get("/",  async (req,res) => await getAllProducts(req,res))
-productRoutes.get("/",  async (req,res) => await getByNameHandler(req,res))
 productRoutes.post("/", async (req,res) => await postProductHandler(req,res) )
 
